0e52ad307b586048d2d1e58a95d8f03b2b946993
Iván Lutin <ivanlutin@gmail.com>
2014-07-28 14:37:38 -0600

    ultimo cambio

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+$(document).ready(init);
 
+function init(){
+	$("#mi_libro").booklet(
+		{width:800,
+		height:300,
+		closed:true,
+		covers:true,
+		autoCenter:true,
+		pagePadding:0,
+		hoverWidth:100,
+
+		}
+		);
+	$("#bt_go").click(function(){
+		$("#mi_libro").booklet("gotopage",$("#in_go").val())
+	});	
+}
